0a5b77cd88fc052727aede02dbc04eb7a403d1cb
Gjermund Ask <me@gjask.com>
2016-03-14 16:07:40 +0100

    More API work, updated README

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-## Requirements
+## Timeplan
+
+A fetcher script and API for timetables at the University of Agder.
+
+### Requirements
 	requests  
 	BeautifulSoup4
 	dateutil
-	lxml
+	lxmli
+
+## API
+
+**Additional dependencies:**  
+	flask
+
+Not finished yet, but queries the database for timetable information.  
+Syntax:  
+	/<subjectcode>/<week>  
+
+If week is anything other than a number it'll default to the current week.  
+Subject codes can be retrieved with the get\_subject\_codes function in timeplan.py (will add an api for this as well)
+
+
+## TODO
+
+- Better info gathering
+- API for subject codes
+- Make a Subjects table
+- Make a sample webpage for displaying timetables
--- api.py
+++ api.py
@@ -1,41 +1,59 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify
 import sqlite3 as sql
+import datetime as dt
 import os
 
 ver = "1.0"
 
 app = Flask(__name__)
 
+# Filter out any disallowed special characters from table name (to avoid injection), wrap in quotation marks
 def clean_table_name(tab):
 	ret = ''.join(_ for _ in tab if _.isalnum() or _ in ".,æøå# ".decode("utf-8"))
 	return unicode("\"" + ret + "\"")
 
-def fetch_from_db(subject_code, week):
+# Build a query to fetch the actual timetable
+def timetable_query(subject_code, week):
+	return "SELECT Weekday, Date, StartTime, EndTime, Course, Type, Info, Campus, Rooms FROM %s \
+		WHERE Week = %s" % (clean_table_name(subject_code), str(week))
+
+# Return all saved subjects, or just the info for one if a subject code is specified
+def subjects_query(subject=None):
+	query = "SELECT * FROM Subjects"
+	if subject: query += " WHERE Code = %s" % (clean_table_name(subject))
+	return query
+
+# Runs a database query. (be careful)
+def fetch_from_db(query):
 	try:
 		db_con = sql.connect("timetable.db")
 		db_con.text_factory = str
 		with db_con:
 			cur = db_con.cursor()
-			cur.execute("SELECT * FROM " + clean_table_name(subject_code) + " WHERE Week = ?", (str(week),))
+			cur.execute(query)
 			data = cur.fetchall()
+
 		return data
+
 	except sql.Error, e:
 		print "SQL error:", str(e)
 
 @app.route('/<subject>/<week>')
 def index(subject="", week=1):
+	# Hash sign can be included (needs to be escaped) or omitted
 	if subject[0] != "#" and "SPLUS" in subject: subject = "#" + subject
-	# d = fetch_from_db("#SPLUSE0C745", int(week))
-	d = fetch_from_db(unicode(subject), week)
-	final = ""
-	for row in d:
-		r = ""
-		for _ in row:
-			_ = str(_)
-			r += " | " + _.decode("utf-8")
-		final += r + "<br />"
-	return final
+	
+	# Set valid week 
+	if not week.isdigit(): week = dt.date.today().isocalendar()[1]
+	else: week = int(week)
+	meta = {"week": week,
+		"subject_code": subject,
+		"subject_name": "Not implemented",
+		"last_updated": "Not implemented",
+		"indices": ["Week day", "Date", "Start time", "End time", "Course", "Type", "Info", "Campus", "Rooms"] }
+	data = fetch_from_db(timetable_query(unicode(subject), week))
+	return jsonify({"timeplan": data, "meta": meta }) 
 
 
 if __name__== '__main__':
